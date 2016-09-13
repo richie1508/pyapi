ccaeab91e094f9dfbf63089f8a181862cb55e22f
liyu2014 <liliang@ikaowo.com>
2016-09-13 16:04:39 +0800

    cmt

--- dist.py
+++ dist.py
@@ -5,10 +5,10 @@ import time
 
 def checkdist(trigPin, echoPin):
     #发出触发信号
-    GPIO.output(trigPin,GPIO.HIGH)
+    GPIO.output(trigPin, GPIO.HIGH)
     #保持10us以上
     time.sleep(0.000020)
-    GPIO.output(trigPin,GPIO.LOW)
+    GPIO.output(trigPin, GPIO.LOW)
     while not GPIO.input(echoPin):
         pass
     #发现高电平时开时计时
@@ -26,10 +26,11 @@ echoPin = 12
 GPIO.setup(trigPin, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(echoPin, GPIO.IN)
 
-time.sleep(2)
+print 'Go!'
+time.sleep(1)
 try:
     while True:
-        print 'Distance: %0.2f m' % checkdist(trigPin, echoPin)
-        time.sleep(0.5)
+        print 'Distance: %0.3f m' % checkdist(trigPin, echoPin)
+        time.sleep(1)
 except KeyboardInterrupt:
     GPIO.cleanup()
